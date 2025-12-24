7e69d3352c43b31bd0b083c1ec01622d849a2dbc
ayushtibra <aayushtibra1997@gmail.com>
2025-12-24 17:59:39 +0530

    add drag and drop functionality

--- next-practise/my-app/Components/TrelloTodo/ListComponent.tsx
+++ next-practise/my-app/Components/TrelloTodo/ListComponent.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Tasks from "./Tasks";
 
-const ListComponent = ({ list, addTaskInList, editListName, editTaskInList, handleTaskDelete }) => {
+const ListComponent = ({ list, addTaskInList, editListName, editTaskInList, handleTaskDelete, setDraggedTask, draggedTask, moveTask }) => {
     const [taskText, setTaskText] = useState('');
     const [isEditing, setIsEditing] = useState(false);
     const [listName, setListName] = useState(list.listName);
@@ -12,7 +12,18 @@ const ListComponent = ({ list, addTaskInList, editListName, editTaskInList, hand
     };
 
     return (
-        <div className="p-4 bg-black text-white mt-2">
+        <div className="p-4 bg-black text-white mt-2"
+            onDragOver={(e) => e.preventDefault()}
+            onDrop={() => {
+                if (!draggedTask) return;
+
+                moveTask(
+                    draggedTask.taskId,
+                    draggedTask.sourceListId,
+                    list.listId
+                );
+            }}
+        >
             <div className="flex justify-between">
                 {isEditing ? (
                     <>
@@ -41,6 +52,7 @@ const ListComponent = ({ list, addTaskInList, editListName, editTaskInList, hand
                         listId={list.listId}
                         editTaskInList={editTaskInList}
                         handleTaskDelete={handleTaskDelete}
+                        setDraggedTask={setDraggedTask}
                     />
                 ))}
                 <input required type="text" placeholder="Add List Name" onChange={(e) => setTaskText(e.target.value)} value={taskText} />
