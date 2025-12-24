51d393e366cb6e1fbc7df39c71c37f3482be9683
ayushtibra <aayushtibra1997@gmail.com>
2025-12-24 15:39:52 +0530

    trello component wiht list edit, add list, task edit, add and delete

---
+++ next-practise/my-app/pages/trelloTodo.tsx
@@ -0,0 +1,11 @@
+import TrelloTodos from "@/Components/TrelloTodo";
+
+const TrelloTodo = () => {
+    return (
+        <>
+            <TrelloTodos />
+        </>
+    )
+}
+
+export default TrelloTodo;
