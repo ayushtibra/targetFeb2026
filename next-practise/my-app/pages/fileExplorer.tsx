1f0f5e407df7e14ecca1c81da52e0f66732529bf
ayushtibra <aayushtibra1997@gmail.com>
2025-12-28 09:31:41 +0530

    file explorer like vs code with add remove feature

---
+++ next-practise/my-app/pages/fileExplorer.tsx
@@ -0,0 +1,12 @@
+import FolderExplorer from '@/Components/folderVSCode';
+import React from 'react';
+
+const fileExplorer = () => {
+    return (
+        <div>
+            <FolderExplorer />
+        </div>
+    );
+};
+
+export default fileExplorer;
