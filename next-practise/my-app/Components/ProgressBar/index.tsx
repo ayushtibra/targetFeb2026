b387fbfb83cfa6d51697ccd4e1d9909ca8316867
ayushtibra <aayushtibra1997@gmail.com>
2025-12-29 09:30:48 +0530

    animated progres bar css

--- next-practise/my-app/Components/ProgressBar/index.tsx
+++ next-practise/my-app/Components/ProgressBar/index.tsx
@@ -11,7 +11,7 @@ const Progress = ({progress}) => {
 
     return (
         <div className="border rounded-xl w-full max-w-[1000px] m-auto mt-4 overflow-hidden">
-            <div style={{width: `${animatedProgress}%`}} className={`p-2 text-center bg-gray-400 transition duration-500 ease-in`}>{animatedProgress}%</div>
+            <div style={{width: `${animatedProgress}%`, transition: '0.5s ease-in'}} className={`p-2 text-center bg-gray-400`}>{progress}%</div>
         </div>
     )
 }
