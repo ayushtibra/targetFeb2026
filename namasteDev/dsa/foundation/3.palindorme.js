c128e32fe974e121b36a1ca4a1d6df2f09e32e7c
ayushtibra <aayushtibra1997@gmail.com>
2026-01-30 12:30:56 +0530

    dsa namastedev

---
+++ namasteDev/dsa/foundation/3.palindorme.js
@@ -0,0 +1,39 @@
+function palindrom(x){
+    const arrX = String(x).split('');
+    for(let i=1; i<arrX.length;i++){
+        if(arrX[i - 1] != arrX[arrX.length - i]){
+            return false;
+        } 
+
+        if(i > arrX.length/2){
+            break
+        }
+    }
+
+    return true
+
+    // or
+
+    // if (x < 0) return false;
+
+    // const s = String(x);
+    // return s === s.split("").reverse().join("");
+
+    // or
+
+//     if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+//   let reversedHalf = 0;
+
+//   while (x > reversedHalf) {
+//     reversedHalf = reversedHalf * 10 + (x % 10);
+//     x = Math.floor(x / 10);
+//   }
+
+//   // For even digits: x === reversedHalf
+//   // For odd digits: x === Math.floor(reversedHalf / 10)
+//   return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+}
+
+const result = palindrom(12121);
+console.log('first', result)
