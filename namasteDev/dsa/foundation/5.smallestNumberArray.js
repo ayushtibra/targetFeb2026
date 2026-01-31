81e152e5fd13259509a73d871e1a3e00dd706bd5
ayushtibra <aayushtibra1997@gmail.com>
2026-01-31 17:32:57 +0530

    5 dsa namastedev

---
+++ namasteDev/dsa/foundation/5.smallestNumberArray.js
@@ -0,0 +1,26 @@
+function smallestNumberArray(arr){
+    if (!Array.isArray(arr)) return false;
+
+    for(let i=0;i<arr.length;i++){
+        if(typeof arr[i] != "number" || !Number.isFinite(arr[i])){
+            return false;
+        }
+    }
+
+    if(arr.length == 0){
+        return null;
+    }
+
+    let smallest = 999999999999999
+    for(let i = 0; i< arr.length;i++){
+        if(arr[i] < smallest ){
+            smallest = arr[i]
+        }
+    }
+    
+    return smallest
+
+}
+
+const result = smallestNumberArray([-4,-1,2,3]);
+console.log('res',result)
