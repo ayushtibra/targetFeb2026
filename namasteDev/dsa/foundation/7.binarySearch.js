6f1432e79b2c14e7704c15d96e2be5922d337bc5
ayushtibra <aayushtibra1997@gmail.com>
2026-02-03 12:13:20 +0530

    7 dsa namastedev

--- namasteDev/dsa/foundation/7.binarySearch.js
+++ namasteDev/dsa/foundation/7.binarySearch.js
@@ -1,17 +1,31 @@
 function BinarySearch(arr, target){
-    const middleArrIndex = Math.floor(arr.length / 2);
+    let left = 0,right = arr.length - 1;
 
-    for(let i=(arr[middleArrIndex] < target ? middleArrIndex : 0); i < (arr[middleArrIndex] < target ? arr.length : middleArrIndex+1 ); i++ ){
-        if(arr[i] == target){
-            return i
-        }
+    while(left <= right){
+        const mid = Math.floor((left + right) / 2);
+        if(arr[mid] == target) return mid;
+
+        if(arr[mid] < target) left = mid + 1
+        else right = mid - 1;
     }
 
     return -1;
 
+    // or
+
+    // const middleArrIndex = Math.floor(arr.length / 2);
+
+    // for(let i=(arr[middleArrIndex] < target ? middleArrIndex : 0); i < (arr[middleArrIndex] < target ? arr.length : middleArrIndex+1 ); i++ ){
+    //     if(arr[i] == target){
+    //         return i
+    //     }
+    // }
+
+    // return -1;
+
 }
 
-const result = BinarySearch([-1,0,3,5,9,12], 2);
+const result = BinarySearch([-1,0,3,5,9,12,15], 9);
 console.log('result', result)
 
 // https://leetcode.com/problems/binary-search/description/
