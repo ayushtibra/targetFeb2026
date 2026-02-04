a5064fee7bbabf2064480a52fe83ef154217f234
ayushtibra <aayushtibra1997@gmail.com>
2026-02-04 19:21:01 +0530

    5 dsa namastedev

---
+++ namasteDev/dsa/array/5.MergeSortedArray.js
@@ -0,0 +1,20 @@
+function MergeSortedArray(nums1,m,nums2,n){
+    let i = m - 1; // 2  
+    let j = n - 1;    // 2 
+    let k = m + n - 1; // 5
+
+    while (j >= 0) { // 2,1, 0
+        if (i >= 0 && nums1[i] > nums2[j]) {  // 3 > 6, 3 > 5, 3 > 2, 2 > 2
+            nums1[k] = nums1[i]; // [1,2,3,3,5,6]
+            i--;
+        } else {
+            nums1[k] = nums2[j]; // [1,2,3,0,0,6], [1,2,3,0,5,6], [1,2,2,3,5,6]
+            j--;
+        }
+        k--;
+    }
+
+}
+
+const result = MergeSortedArray([1,2,3,0,0,0],3,[2,5,6],3);
+console.log(result);
