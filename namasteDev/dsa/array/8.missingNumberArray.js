e312d68507cb5825fcae15e24345e08728abeced
ayushtibra <aayushtibra1997@gmail.com>
2026-02-06 13:11:51 +0530

    8 dsa namastedev

---
+++ namasteDev/dsa/array/8.missingNumberArray.js
@@ -0,0 +1,30 @@
+function MissingNumberArray(nums) {
+    const range = nums.length;
+    const numberInRange = {};
+
+    for (let i = 0; i <= range; i++) {
+        numberInRange[i] = false
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!numberInRange[nums[i]]) {
+            numberInRange[nums[i]] = true
+        }
+    }
+
+    let missing;
+
+    for (const key in numberInRange) {
+        if (numberInRange[key] === false) {
+            missing = Number(key);
+            break;
+        }
+    }
+
+    return missing
+
+    console.log('first', missing)
+
+}
+
+MissingNumberArray([3, 0, 1]);
