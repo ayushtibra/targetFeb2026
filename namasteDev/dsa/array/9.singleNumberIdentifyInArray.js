6dd73d7c638d9bfe78b1b075b594d4f4301c9cc1
ayushtibra <aayushtibra1997@gmail.com>
2026-02-06 17:47:51 +0530

    9 dsa array namastedev

---
+++ namasteDev/dsa/array/9.singleNumberIdentifyInArray.js
@@ -0,0 +1,20 @@
+function SingleNumberIdentifyInArray(nums){
+    const mapNumber = {};
+
+    for(let i = 0; i < nums.length; i++){
+        if(!mapNumber[nums[i]]){
+            mapNumber[nums[i]] = 1
+        } else { 
+            mapNumber[nums[i]] = mapNumber[nums[i]] + 1
+        }
+    }
+
+    for (let num in mapNumber) {
+        if(mapNumber[num] == 1){
+            return num;
+        }
+    }
+}
+
+const result = SingleNumberIdentifyInArray([4,1,2,1,2]);
+console.log('first', result)
