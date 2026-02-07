a62890fddd427e8400f9eaa7fc2f97e390bca279
ayushtibra <aayushtibra1997@gmail.com>
2026-02-07 19:59:14 +0530

    1 string dsa namastedev

---
+++ namasteDev/dsa/string/1.lengthOfLastWord.js
@@ -0,0 +1,16 @@
+function LengthOfLastWord(s){
+    let length = 0;
+    let startCharacter = 0;
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i] != ' '){
+            startCharacter = 1;
+            length++;
+        } else if(startCharacter > 0 && s[i] == ' '){
+            break;
+        }
+    }
+
+    console.log('first', length)
+}
+
+const result = LengthOfLastWord("luffy is still joyboy");
