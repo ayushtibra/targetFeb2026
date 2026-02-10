6ede3777221e8988ac5b42b6c3181ce400aca618
ayushtibra <aayushtibra1997@gmail.com>
2026-02-10 22:01:21 +0530

    10 string dsa namastedev

---
+++ namasteDev/dsa/string/10.isAnagram.js
@@ -0,0 +1,32 @@
+function isAnagram(s,t){
+    if(s.length != t.length) return false;
+    const sHash = {};
+
+    for(let ch of s){
+        if(!sHash[ch]){
+            sHash[ch] = 1;
+        } else {
+            sHash[ch] += 1;
+        }
+    }
+
+    for (let ch of t){
+        if(sHash[ch]){
+            sHash[ch] = sHash[ch] - 1;
+        } else{
+            return false;
+        }
+    }
+
+    for(let ch in sHash){
+        if(sHash[ch] != 0){
+            return false
+        } else {
+            return true;
+        }
+    }
+
+}
+
+const result = isAnagram('anagram','nagaram');
+console.log('result', result)
