5068928865d8d8bffe8749c036a8fb49e569318d
ayushtibra <aayushtibra1997@gmail.com>
2026-02-14 18:15:33 +0530

    changes

---
+++ namasteDev/dsa/binarysearch/1.squareRootOfNumber(REVISIT).js
@@ -0,0 +1,17 @@
+function mySqrt(x){
+    if(x == 0) return 0;
+    let left = 1;
+    let right = x;
+
+    while(left <= right){
+        let middleNum = Math.floor((left+right)/2);
+        if((middleNum * middleNum) == x ) return middleNum;
+        if((middleNum * middleNum) > x)  right = middleNum - 1;
+        if((middleNum * middleNum) < x)  left = middleNum + 1;
+    }
+
+    return right
+}
+
+const result = mySqrt(9);
+console.log('first',result)
