dc2c6bab232f2d21d714b8941b3b8c064e4f9520
ayushtibra <aayushtibra1997@gmail.com>
2026-02-20 11:32:14 +0530

    add merge sort algo

--- namasteDev/dsa/array/5.MergeSortedArray(REVISIT).js
+++ namasteDev/dsa/array/5.MergeSortedArray(REVISIT).js
@@ -21,3 +21,42 @@ console.log(result);
 
 // https://leetcode.com/problems/merge-sorted-array/description/
 // Topics - two pointers
+
+function mergeSortedArrays(arr1, arr2) {
+  let mergedArray = [];
+  let i = 0; // Pointer for arr1
+  let j = 0; // Pointer for arr2
+  const len1 = arr1.length;
+  const len2 = arr2.length;
+
+  // While both arrays have elements to compare
+  while (i < len1 && j < len2) {
+    if (arr1[i] < arr2[j]) {
+      mergedArray.push(arr1[i]);
+      i++;
+    } else {
+      mergedArray.push(arr2[j]);
+      j++;
+    }
+  }
+
+  // Add remaining elements from arr1 (if any)
+  while (i < len1) {
+    mergedArray.push(arr1[i]);
+    i++;
+  }
+
+  // Add remaining elements from arr2 (if any)
+  while (j < len2) {
+    mergedArray.push(arr2[j]);
+    j++;
+  }
+
+  return mergedArray;
+}
+
+// Example usage:
+const array1 = [1, 3, 5, 7];
+const array2 = [2, 4, 6, 8, 10];
+const merged = mergeSortedArrays(array1, array2);
+console.log(merged); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 10]
